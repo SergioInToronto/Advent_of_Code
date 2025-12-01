51f14393cf6cf05ce15aee3a8c35072f6b2e28da
Sergio <fake@unreal.com>
2025-12-01 12:04:39 -0500

    Add 2022

--- rust/hello_cargo/target/debug/deps/hello_cargo-434667d17781793e.d
+++
@@ -1,5 +0,0 @@
-/home/tvcomputer/dev/Advent-of-Code-2022/rust/hello_cargo/target/debug/deps/hello_cargo-434667d17781793e: src/main.rs
-
-/home/tvcomputer/dev/Advent-of-Code-2022/rust/hello_cargo/target/debug/deps/hello_cargo-434667d17781793e.d: src/main.rs
-
-src/main.rs:
--- rust/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/tvcomputer/dev/Advent-of-Code-2022/rust/hello_cargo/target/debug/hello_cargo: /home/tvcomputer/dev/Advent-of-Code-2022/rust/hello_cargo/src/main.rs
