7dfe3387cbcf68f5883f6625db28d7e0db49c36b
Sergio <fake@unreal.com>
2025-12-04 21:44:47 -0500

    Day 2 part 1

---
+++ 2025/02/day2.exs
@@ -0,0 +1,48 @@
+defmodule Day2 do
+  def part1 do
+    "input.txt"
+    |> File.read!()
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(&procecss_range/1)
+    |> List.flatten()
+    |> Enum.sum()
+    |> IO.inspect(label: "Sum")
+  end
+
+  def procecss_range(range_str) do
+    [min, max] = range_str |> String.split("-") |> Enum.map(&String.to_integer/1)
+    # int_min = String.to_integer(min)
+    # int_max = String.to_integer(max)
+    # IO.puts("Count: #{inspect(int_max - int_min)}")
+    IO.puts("Count: #{inspect(max - min)}")
+
+    Enum.reduce(min..max, [], &keep_if_invalid/2)
+  end
+
+  def keep_if_invalid(num, acc) do
+    digit_count = num |> Integer.to_string() |> String.length()
+    if digit_count |> rem(2) == 0 and valid?(num, digit_count) do
+      [num | acc]
+    else
+      acc
+    end
+  end
+
+  def valid?(num, digit_count) do
+    exponent = div(digit_count, 2)
+    factor = 10 |> :math.pow(exponent) |> trunc()
+    first_half = num |> div(factor)
+    second_half = num |> rem(factor)
+
+    # if num == 989989 do
+    #   dbg({num, digit_count, factor, first_half, second_half, first_half == second_half})
+    #   raise "STOP"
+    # end
+
+    first_half == second_half
+  end
+end
+
+
+Day2.part1()
