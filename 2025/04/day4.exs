e48ebe6e4eda4eabc3b14d1344326a9b2607b54e
Sergio <fake@unreal.com>
2025-12-09 23:39:47 -0500

    Day 4 part 2 ?

--- 2025/04/day4.exs
+++ 2025/04/day4.exs
@@ -1,5 +1,15 @@
-defmodule Day4.Part1 do
-  def run do
+defmodule Day4 do
+  def part1() do
+    {world, bounds} = load()
+
+    world
+    |> Enum.filter(&has_roll?/1)
+    |> Enum.filter(& fewer_than_four_neighbors?(&1, world, bounds))
+    |> Enum.count()
+    |> IO.inspect(label: "Rolls with fewer than 4 neighbours")
+  end
+
+  def load() do
     lines =
       "input.txt"
       |> File.read!()
@@ -8,13 +18,9 @@ defmodule Day4.Part1 do
     width = lines |> hd() |> String.length()
     height = length(lines)
     bounds = {width, height}
+    world = to_map(lines)
 
-    lines
-    |> to_map()
-    |> Enum.filter(&has_roll?/1)
-    |> Enum.filter(& fewer_than_four_neighbors?(&1, world, bounds))
-    |> Enum.count()
-    |> IO.inspect(label: "Rolls with fewer than 4 neighbours")
+    {world, bounds}
   end
 
   def to_map(lines) do
@@ -60,6 +66,35 @@ defmodule Day4.Part1 do
   end
 
 
+  def part2() do
+    {world, bounds} = load()
+
+    world
+    |> remove_rolls_until_zero(bounds, 0)
+    |> IO.inspect(label: "Total rolls recursively")
+  end
+
+  def remove_rolls_until_zero(world, bounds, count) do
+    coords = coords_of_accessable_rolls(world, bounds)
+
+    if coords == [] do
+      count
+    else
+      new_world = Enum.reduce(coords, world, fn coord, acc ->
+        Map.put(acc, coord, false)
+      end)
+
+      remove_rolls_until_zero(new_world, bounds, count + length(coords))
+    end
+  end
+
+  def coords_of_accessable_rolls(world, bounds) do
+    world
+    |> Enum.filter(&has_roll?/1)
+    |> Enum.filter(& fewer_than_four_neighbors?(&1, world, bounds))
+    |> Enum.map(fn {{x, y}, _value} -> {x, y} end)
+  end
 end
 
-Day4.Part1.run()
+Day4.part1()
+Day4.part2()
