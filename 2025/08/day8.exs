0284cb64b0077a5cebee9cbee9c11a153ae97b03
Sergio <fake@unreal.com>
2025-12-23 14:14:18 -0500

    Fix 2 bugs when merging circuits. Works now

--- 2025/08/day8.exs
+++ 2025/08/day8.exs
@@ -19,16 +19,17 @@ defmodule Day8 do
           {distance, MapSet.new([box, neighbour])}
         end)
       end)
-      |> Enum.sort_by(&elem(&1, 0))
       # uniq_by to remove duplicates in different order, like {A, B} and {B, A}
       |> Enum.uniq_by(&elem(&1, 1))
+      |> Enum.sort_by(&elem(&1, 0))
       |> Enum.slice(0..999)
+      |> IO.inspect(printable_limit: :infinity, limit: :infinity)
       |> Enum.map(fn {_distance, pair} -> MapSet.to_list(pair) end)
 
     circuits = connect_all_junction_boxes(nearest_box_pairs)
 
     circuits
-    |> Enum.map(&(&1 |> MapSet.to_list() |> length()))
+    |> Enum.map(&MapSet.size(&1))
     |> Enum.sort()
     |> Enum.reverse()
     |> Enum.slice(0..2)
@@ -38,6 +39,9 @@ defmodule Day8 do
 
     # 480 is too low (10 * 8 * 6)
     # 2366 is too low (14 * 13 * 13)
+    # 2904 is wrong :(
+    # 11830 is too low (random guess)
+    # 59150 is wrong (random guess)
   end
 
   def compute_distance(box1, box2) when box1 == box2, do: :inf
@@ -45,23 +49,44 @@ defmodule Day8 do
   def compute_distance(box1, box2) do
     {x, y, z} = box1
     {bx, by, bz} = box2
-    :math.sqrt((bx - x) ** 2 + (by - y) ** 2 + (bz - z) ** 2)
+    :math.sqrt(((bx - x) ** 2) + ((by - y) ** 2) + ((bz - z) ** 2))
   end
 
   def connect_all_junction_boxes(pairs) do
     pairs
     |> Enum.reduce([], fn [box1, box2], circuits ->
-      if circuit_index = Enum.find_index(circuits, fn c -> box1 in c or box2 in c end) do
-        update_in(circuits, [Access.at(circuit_index)], &add_boxes_to_circuit(&1, box1, box2))
-      else
-        new_circuit = MapSet.new([box1, box2])
-
-        [new_circuit | circuits]
+      box1_circuit = Enum.find_index(circuits, fn c -> box1 in c end)
+      box2_circuit = Enum.find_index(circuits, fn c -> box2 in c end)
+      case {box1_circuit, box2_circuit} do
+        {nil, nil} -> new_circuit(circuits, box1, box2)
+        {index, nil} -> add_boxes_to_circuits(circuits, index, box1, box2)
+        {nil, index} -> add_boxes_to_circuits(circuits, index, box1, box2)
+        {index1, index2} when index1 == index2 -> circuits
+        {index1, index2} -> merge_circuits(circuits, index1, index2)
       end
     end)
   end
 
+  def new_circuit(circuits, box1, box2) do
+    new_circuit = MapSet.new([box1, box2])
+
+    [new_circuit | circuits]
+  end
+
+  def merge_circuits(circuits, index1, index2) do
+    circuit2 = Enum.at(circuits, index2)
+
+    circuits
+    |> update_in([Access.at(index1)], & MapSet.union(&1, circuit2))
+    |> List.delete_at(index2)
+  end
+
+  def add_boxes_to_circuits(circuits, index, box1, box2) do
+    update_in(circuits, [Access.at(index)], &add_boxes_to_circuit(&1, box1, box2))
+  end
+
   def add_boxes_to_circuit(circuit, box1, box2) do
+    # Caught an early mistake
     if box1 in circuit and box2 in circuit and MapSet.size(circuit) == 2 do
       raise("Somehow adding the same two boxes to existing circuit")
     end
@@ -70,4 +95,5 @@ defmodule Day8 do
   end
 end
 
-Day8.part1()
+# Day8.part1()
+Day8.part2()
