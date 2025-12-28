3b6cd29be2c6694a44df4bf15684eb95ed6d1fce
Sergio <fake@unreal.com>
2025-12-28 10:42:38 -0500

    Day 1 part 2

--- 2025/01/main.exs
+++ 2025/01/main.exs
@@ -10,32 +10,27 @@ defmodule Main do
       "L" <> x -> String.to_integer(x) * -1
     end)
     |> Enum.reduce({@dial_start_position, 0}, &spin_and_count/2)
-    # |> elem(1)
+    |> elem(1)
     |> IO.inspect(label: "Password to open door")
-
-    # 8847 is too high.
-    # 6707 is too high.
-    # 6546 is wrong :'(
-    # 6165 is too low.
   end
 
   def spin_and_count(move, {pos, count}) do
-    new_pos = pos + move
-    full_rotations = new_pos |> div(100) |> abs()
-    new_count = count + full_rotations
+    move_without_full_rotations = rem(move, 100)
+    pos_raw = (pos + move_without_full_rotations)
+    {new_pos, passed_or_landed_on_zero_addr} = fix_pos(pos_raw)
+    full_rotations = move |> div(100) |> abs()
+    # Prevent double-counting the zero
+    edge_case_fix_addr = if pos == 0 and move < 0, do: -1, else: 0
+    new_count = count + full_rotations + passed_or_landed_on_zero_addr + edge_case_fix_addr
 
-    fixed_new_pos = fix_pos(new_pos)
-
-    # if count ends on zero, +1 count
-    new_count = if fixed_new_pos == 0, do: new_count + 1, else: new_count
-
-    {fixed_new_pos, new_count}
+    {new_pos, new_count}
   end
 
-  def fix_pos(pos) when pos > 100, do: fix_pos(pos - 100)
-  def fix_pos(pos) when pos < 0, do: fix_pos(pos + 100)
-  def fix_pos(100), do: 0
-  def fix_pos(pos), do: pos
+  def fix_pos(0), do: {0, 1}
+  def fix_pos(100), do: {0, 1}
+  def fix_pos(x) when x > 100, do: {x - 100, 1}
+  def fix_pos(x) when x < 0, do: {x + 100, 1}
+  def fix_pos(x), do: {x, 0}
 end
 
 Main.run()
