01fe508282813f56d82107205f75082423a919a7
Sergio <fake@unreal.com>
2025-12-28 12:50:45 -0500

    day2 part2

--- 2025/02/day2.exs
+++ 2025/02/day2.exs
@@ -19,6 +19,7 @@ defmodule Day2 do
 
   def keep_if_invalid(num, acc) do
     digit_count = num |> Integer.to_string() |> String.length()
+
     if digit_count |> rem(2) == 0 and valid?(num, digit_count) do
       [num | acc]
     else
@@ -34,63 +35,56 @@ defmodule Day2 do
 
     first_half == second_half
   end
-end
 
-defmodule Day2part2 do
-  def run do
+  def part2 do
     "input.txt"
     |> File.read!()
     |> String.trim()
     |> String.split(",")
-    |> Enum.map(&procecss_range/1)
+    |> Enum.map(&procecss_range_part2/1)
     |> List.flatten()
     |> Enum.sum()
     |> IO.inspect(label: "Sum Part 2")
+    # 1067619439706031 is too high
+    # 20077273032 is too high
+    # 20077272987
   end
 
-  def procecss_range(range_str) do
+  def procecss_range_part2(range_str) do
     [min, max] = range_str |> String.split("-") |> Enum.map(&String.to_integer/1)
-    IO.puts("Count: #{inspect(max - min)}")
+    IO.inspect(min..max)
+    IO.puts("\tCount: #{inspect(max - min)}")
 
-    Enum.reduce(min..max, [], &keep_if_invalid/2)
+    Enum.reduce(min..max, [], &keep_if_invalid_part2/2)
   end
 
-  def keep_if_invalid(num, acc) do
-    digit_count = num |> Integer.to_string() |> String.length()
-    if valid?(num, digit_count) do
+  def keep_if_invalid_part2(num, acc) do
+    num_str = Integer.to_string(num)
+    digit_count = String.length(num_str)
+
+    if part2_invalid?(num_str, digit_count) do
       [num | acc]
     else
       acc
     end
   end
 
-  def valid?(num, digit_count) do
-    factors
-
-    exponent = div(digit_count, 2)
-    factor = 10 |> :math.pow(exponent) |> trunc()
-    first_half = num |> div(factor)
-    second_half = num |> rem(factor)
-
-    first_half == second_half
+  # single-digit numbers do not count as repeating patterns
+  def part2_invalid?(_, 1), do: false
+  def part2_invalid?(num_str, digit_count) do
+    max_pattern_length = max(1, div(digit_count, 2))
+    # Check for patterns of 1 digit up to half the length of the number (eg: length 5 -> max 2-digit patterns)
+    1..max_pattern_length
+    |> Enum.find(false, fn length ->
+      repeating_groups_of_n(num_str, length)
+    end)
   end
 
-  def factors(1, _), do: [1]
-  def factors(_, 1), do: [1]
-  def factors(n, i) do
-    if rem(n, i) == 0 do
-      [ i | factors(n, i-1) ]
-    else
-      factors(n, i-1)
-    end
-  end
-
-  def is_prime?(n) do
-    factors(n, div(n, 2)) == [1]
+  def repeating_groups_of_n(num_str, length) do
+    pattern = String.slice(num_str, 0, length) |> String.graphemes()
+    num_str |> String.graphemes() |> Enum.chunk_every(length, length, []) |> Enum.all?(&(&1 == pattern))
   end
 end
 
-
 # Day2.part1()
-# I'm still figuring out naming and don't care about consistency right now
-Day2part2.run()
+Day2.part2()
