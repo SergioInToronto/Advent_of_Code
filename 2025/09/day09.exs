e572601a08f336c308cb3fe4e1ead17b86bb35da
Sergio <fake@unreal.com>
2026-01-02 16:29:21 -0500

    WIP day9 part2

--- 2025/09/day09.exs
+++ 2025/09/day09.exs
@@ -27,9 +27,39 @@ defmodule Day9 do
     x = abs(x1 - x2) + 1
     y = abs(y1 - y2) + 1
     x * y
+  end
 
-    # {x * y, [x1, y1], [x2, y2]}
+  ###########################################################
+
+  def part2 do
+    # "input.txt"
+    coords =
+      "experiments"
+      |> File.read!()
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse_coordinates/1)
+      |> draw_and_save()
+
+    # pairs = for x <- coords, y <- coords, x != y, do: {x, y}
+  end
+
+  def draw_and_save(coords) do
+    file = File.open!("display.txt", [:write])
+
+    # for x <- 0..100_000, y <- 0..100_000 do
+    for x <- 1..14, y <- 1..14 do
+      if rem(x, 2000) == 1 and y == 1, do: IO.puts("Line #{x}...")
+      if y == 1, do: IO.write(file, "\n")
+
+      if [x, y] in coords do
+        IO.inspect([x,y], charlists: :as_lists)
+        IO.write(file, "#")
+      else
+        IO.write(file, ".")
+      end
+    end
   end
 end
 
-Day9.part1()
+# Day9.part1()
+Day9.part2()
