ae73caf823a804d70394f140af7ed364a745a7a6
Sergio <fake@unreal.com>
2026-01-06 23:02:10 -0500

    WIP day12 part2

--- 2025/12/day12.exs
+++ 2025/12/day12.exs
@@ -1,3 +1,12 @@
+defmodule Region do
+  @enforce_keys [:width, :height]
+  @allowed_keys [:width, :height, :shapes]
+  @type t :: %__MODULE__{width: non_neg_integer, height: non_neg_integer, grid: nil, shapes: []}
+
+  defstruct [:width, :height, :grid, shapes: []]
+
+end
+
 defmodule Day12 do
   # Hard-code my 6 shapes for this puzzle.
   # While I normally try to make everything dynamic, I feel a little
@@ -8,8 +17,6 @@ defmodule Day12 do
       # - #.#
       # - ###
       # - #.#
-      # TODO: need this?
-      index: 0,
       shape: [true, false, true, true, true, true, true, false, true],
       # TODO: will bother using this optimization?
       flip: false,
@@ -19,7 +26,6 @@ defmodule Day12 do
       # - ###
       # - ###
       # - #..
-      index: 1,
       shape: [true, true, true, true, true, true, true, false, false],
       flip: true,
       rotations: 4
@@ -28,7 +34,6 @@ defmodule Day12 do
       # - ###
       # - .##
       # - ##.
-      index: 2,
       shape: [true, true, true, false, true, true, true, true, false],
       flip: true,
       rotations: 4
@@ -37,7 +42,6 @@ defmodule Day12 do
       # - ###
       # - .##
       # - ..#
-      index: 3,
       shape: [true, true, true, false, true, true, false, false, true],
       flip: true,
       rotations: 4
@@ -46,7 +50,6 @@ defmodule Day12 do
       # - ###
       # - ..#
       # - ###
-      index: 4,
       shape: [true, true, true, false, false, true, true, true, true],
       flip: false,
       rotations: 4
@@ -62,6 +65,8 @@ defmodule Day12 do
     },
   ]
 
+  @region_regex ~r/([0-9]{1,2})x([0-9]{1,2}): ([0-9 ]+)/
+
   def part1 do
     # "input.txt"
     "experiments.txt"
@@ -71,13 +76,38 @@ defmodule Day12 do
     |> String.split("\n", trim: true)
     |> Enum.with_index(31)
     |> Enum.map(&can_fix_all_presents?/1)
-    |> dbg()
+    |> Enum.count(& &1)
+    |> IO.inspect(label: "Regions that fit all presents")
   end
 
   def can_fix_all_presents?({entry, line_no}) do
-    # TODO: YOU ARE HERE
+    {width, height, required_presents} = parse_region(entry)
+
+    initial_region = for x <- 0..(width - 1), y <- 0..(height - 1), do: false
+
+    fit_shapes(initial_region)
+
+    line = line_no |> Integer.to_string() |> String.pad_leading(4)
+    IO.puts("#{line_no}: #{entry}  -->  #{inspect(initial_region)}")
 
     false
+  end
+
+  def parse_region(text) do
+    [_, width, height, required_presents] = Regex.run(@region_regex, text)
+    width = String.to_integer(width)
+    height = String.to_integer(height)
+    required_presents = required_presents |> String.split() |> Enum.map(&String.to_integer/1)
+
+    {width, height, required_presents}
+  end
+
+  def new_region()
+
+  # TODO: figure out recursive function signature
+
+  def add_shape_to_region(region, shape) do
+    # TODO: YOU ARE HERE kinda...
   end
 
 end
